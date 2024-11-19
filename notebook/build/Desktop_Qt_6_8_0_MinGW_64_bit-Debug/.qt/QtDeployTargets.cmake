38d09ce3c37ea52c45d483a8138fb2e0805eb046
acer <245313924@qq.com>
2024-11-19 20:11:31 +0800

    v1.0

---
+++ notebook/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_notebook_FILE E:/QTfile/notebook/QTnotebook/notebook/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/notebook.exe)
+set(__QT_DEPLOY_TARGET_notebook_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_notebook_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
---
+++ notebook/widget.cpp
@@ -0,0 +1,18 @@
+#include "widget.h"
+#include "./ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    //虽然上面一行代码进行widget和ui的窗口关联，但是如果发生窗口大小变化的时候，里面的布局不会随之变换
+    //通过下面这行代码进行显示说明，让窗口变化时，布局及其子控件随之变换
+    this->setLayout(ui->verticalLayout);
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
